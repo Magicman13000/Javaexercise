9cf3f76ca9fd83f60a343551b892e73fee1c56e0
unknown <davidruhlman@ROOM316-13.student.rockhurst.int>
2016-04-14 10:53:09 -0500

    OLOF AKA MAGIC

---
+++ Tele.java
@@ -0,0 +1,92 @@
+
+public class Tele {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int = TV;
+		int = tv1;
+		int = tv2;
+		int = tv3;
+		int = vol
+		boolean on;
+		boolean off;
+				
+		TV tv1 = new TV();
+        tv1.turnOn();
+        tv1.setChannel(30);
+        tv1.setVolume(3);
+        
+        TV tv2 = new TV();
+        tv2.turnOn();
+        tv2.channelUp();
+        tv2.channelUp();
+        tv2.volumeUp();
+        
+        TV tv3 = new TV();
+        tv3.setChannel(30);
+        tv3.channelUp();
+        tv3.volumeUp();
+        
+        System.out.println("tv1's channel: " + tv1.channel);
+        System.out.println("tv1's volume:  " + tv1.volume);
+        System.out.println("tv2's channel: " + tv2.channel);
+        System.out.println("tv2's volume:  " + tv2.volume);
+        System.out.println("tv3's channel: " + tv3.channel);
+        System.out.println("tv3's volume:  " + tv3.volume);
+}
+	for(tv1 == on);
+		if(tv1 <= 0)	
+			System.out.println("0");
+		else(tv1 + == 1);
+			System.out.println("50");
+			
+	for(tv == on);
+		if(tv2 <= 0) 	
+			System.out.println("0");
+		else(tv2 + == 1);
+			System.out.println("50");
+			
+	for(tv3 == on);
+		if(tv3 <= 0) 	
+			System.out.println("0");
+		else(tv3 + == 1);
+			System.out.println("50");
+			
+	
+			
+	}
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
